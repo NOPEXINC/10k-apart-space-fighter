c379739311a8805a9dfff5f73f3f6900851bfc0f
Christopher Imrie <christopher.imrie@ormlondon.com>
2016-09-09 00:22:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+10k Canvas & JS Space Fighter
+=============================
+
+Canvas & JS space game under 10kb for the 10k Apart competition.
+
+Optimised for touch and mobile, but fully functional with keyboard and mouse.
+
+Demo: http://cki-10k-apart.s3-website-eu-west-1.amazonaws.com 
+
+
+### Install
+
+1.  Clone the repo
+2.  To install dependencies, run `npm install`
+3.  Run `npm start` to start HTTP server with the game running. Navigate to the URL displayed displayed in the CLI.
+
+### Building
+
+1.  Run `npm run build`
+2   The built assets will be placed in a `build` folder
---
+++ src/js/main.js
@@ -0,0 +1,223 @@
+(function(){
+    var canvas = document.getElementById("c"),
+        ctx = canvas.getContext('2d'),
+        M = Math,
+        fireInterval,
+        rand = M.random,
+        t=0,
+        cfg = {
+            w:0,
+            h:0,
+            fps:60,
+            fireT:200,
+            bgLimit:40,
+            bgv:10, //velocity
+            bgS:3, //size
+            boundFactor:2,
+            bulletV:15,
+            enemyV:1,
+            kmode:false
+        },
+        ship = {
+            s:10, //size
+            x: -100,
+            y: -100,
+            f: false //firing
+        },
+        bounds={max:0, min:0},
+        bullets = [],
+        enemies = [],
+        bg = [];
+
+function init(){
+
+    cfg.w = document.body.offsetWidth;
+    cfg.h = document.body.offsetHeight;
+
+    canvas.width = cfg.w;
+    canvas.height = cfg.h;
+
+    ship.x = cfg.w / 2;
+    ship.y = cfg.h * 0.9;
+
+    bounds.max = cfg.h * cfg.boundFactor - (cfg.h / cfg.boundFactor);
+    bounds.min = -(cfg.h / cfg.boundFactor);
+
+    document.body.addEventListener("touchstart", tstart);
+    document.body.addEventListener("touchmove", tmove);
+    document.body.addEventListener("touchend", tend);
+    document.body.addEventListener("mousedown", mdown);
+    document.body.addEventListener("mousemove", mmove);
+    document.body.addEventListener("mouseup", mup);
+    document.body.addEventListener("keydown", kdown);
+    document.body.addEventListener("keyup", kup);
+
+    //Background
+    for(i=0;i<cfg.bgLimit;i++) {
+        bg.push({
+            x: cfg.w * rand(),
+            y: cfg.boundFactor * cfg.h * rand() - (cfg.h/cfg.boundFactor),
+            z: rand()
+        })
+    }
+
+    setInterval(function () {
+        t++;
+        if(rand() > 0.5){
+            enemies.push({
+                x:rand()*cfg.w,
+                y: -20
+            });
+        }
+    },1000);
+
+
+    setInterval(function(){
+        window.requestAnimationFrame(renderLoop);
+    }, 1000/cfg.fps);
+}
+
+function mdown(e){
+    tstart()
+}
+function tstart(e){
+    ship.f = true;
+    fireInterval = setInterval(fire, cfg.fireT);
+}
+function kdown(e){
+    if(e.keyCode===32){
+        ship.f = true;
+        fire();
+    }
+    if(e.keyCode===37){
+        cfg.kmode = true;
+        ship.x -= 15;
+    }
+    if(e.keyCode===38){
+        cfg.kmode = true;
+        ship.y -= 15;
+    }
+    if(e.keyCode===39){
+        cfg.kmode = true;
+        ship.x += 15;
+    }
+    if(e.keyCode===40){
+        cfg.kmode = true;
+        ship.y += 15;
+    }
+}
+function mmove(e){
+    if(!ship.f) return;
+    tmove({
+        touches:[{pageX:e.pageX, pageY:e.pageY}]
+    })
+}
+function tmove(e){
+    ship.x = e.touches[0].pageX;
+    ship.y = e.touches[0].pageY;
+}
+function mup(e){
+    tend()
+}
+
+function kup(e){
+    if(e.keyCode===32 || (e.keyCode >=37 && e.keyCode <=40)){
+        tend();
+    }
+}
+function tend(e){
+    ship.f = false;
+    clearInterval(fireInterval);
+}
+
+function fire(){
+    bullets.push({
+        x:ship.x,
+        y:ship.y - 20
+    })
+}
+function renderLoop() {
+    ctx.clearRect(0, 0, cfg.w, cfg.h);
+
+    fs(0.4);
+    for(i=0;i<bg.length;i++) {
+
+        bg[i].y = bg[i].y + cfg.bgv * bg[i].z;
+        ctx.beginPath();
+        ctx.arc(bg[i].x, bg[i].y, bg[i].z * cfg.bgS, 0, 2 * M.PI);
+        ctx.fill();
+
+        if(bg[i].y > bounds.max){
+            bg[i].y = bounds.min;
+        }
+    }
+
+    //Ship
+    ls(1, 4);
+    ctx.beginPath();
+    ctx.moveTo(ship.x, ship.y+10);
+    ctx.lineTo(ship.x+10, ship.y+15);
+    ctx.lineTo(ship.x, ship.y-10);
+    ctx.lineTo(ship.x-10, ship.y+15);
+    ctx.lineTo(ship.x, ship.y+10);
+    ctx.stroke();
+
+    //Bullets
+    ls(1, 4);
+    for(i=0;i<bullets.length;i++) {
+        ctx.beginPath();
+        ctx.moveTo(bullets[i].x, bullets[i].y);
+        ctx.lineTo(bullets[i].x, bullets[i].y-20);
+        ctx.stroke();
+
+        bullets[i].y -= cfg.bulletV;
+        if(bullets[i].y < bounds.min){
+            bullets.splice(i, 1);
+            continue;
+        }
+
+        //hit
+        for(j=0;j<enemies.length;j++) {
+            if(M.sqrt(M.pow(bullets[i].x - enemies[j].x,2) + M.pow(bullets[i].y - enemies[j].y,2)) < 20){
+                bullets.splice(i, 1);
+                enemies.splice(j, 1);
+                break;
+            }
+        }
+    }
+    //Enemies
+    ls(1, 4, "255,0,0");
+    for(i=0;i<enemies.length;i++) {
+        ctx.beginPath();
+        ctx.moveTo(enemies[i].x, enemies[i].y-10);
+        ctx.lineTo(enemies[i].x+10, enemies[i].y-15);
+        ctx.lineTo(enemies[i].x, enemies[i].y+10);
+        ctx.lineTo(enemies[i].x-10, enemies[i].y-15);
+        ctx.lineTo(enemies[i].x, enemies[i].y-10);
+        ctx.stroke();
+
+        enemies[i].y += cfg.enemyV;
+        if(enemies[i].y > bounds.max){
+            enemies.splice(i, 1);
+        }
+    }
+
+    //Slowly move ship back to center
+    if(!ship.f && !cfg.kmode){
+        ship.x = ship.x - 0.03 *(ship.x - cfg.w / 2);
+        ship.y = ship.y - 0.03 *(ship.y - cfg.h * 0.9);
+    }
+}
+
+function fs(a){
+    ctx.fillStyle = "rgba(255,255,255,"+a+")";
+}
+function ls(a,b,c){
+    ctx.strokeStyle = "rgba("+(c || "255,255,255")+","+a+")";
+    ctx.lineWidth = b || 1;
+}
+
+//GO!
+init();
+
+})();
